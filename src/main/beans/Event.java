4d5aeaba8ac41692bb62c1aea3c4690ce7ba3b18
Gennadii_Borodin <hakuink@gmail.com>
2017-07-03 10:30:02 +0400

    1.4

---
+++ src/main/beans/Event.java
@@ -0,0 +1,60 @@
+package main.beans;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * Created by Gennadii_Borodin on 6/30/2017.
+ */
+public class Event {
+
+    public Event(){
+        Random random = new Random();
+        id = random.nextInt(50);
+    }
+
+    public Event(Date date, DateFormat df){
+        Random random = new Random();
+        id = random.nextInt(50);
+        this.date = date;
+        this.df = df;
+    }
+
+    private int id;
+    private String msg;
+    private Date date;
+    private DateFormat df;
+
+    @Override
+    public String toString(){
+        String s = "id - " + id + " \n" +
+                "date - " + df.format(date) + " \n" +
+                "mesage - " + msg;
+        return s;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
